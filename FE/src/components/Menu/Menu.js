9fc680b81f05a4e912f618d1800a837d26f23f54
Diego Fernando Jimenez Valderrama <djimenezv@gmail.com>
2021-12-13 21:20:55 -0500

    adding menu

--- FE/src/components/Menu/Menu.js
+++ FE/src/components/Menu/Menu.js
@@ -6,6 +6,9 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Button from '@mui/material/Button';
 import './Menu.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Menu = () => {
 
@@ -16,8 +19,15 @@ const Menu = () => {
             <Button variant="contained" color="primary" onClick={() => setDialogOpen(true)}> 
                 <MenuIcon  role="open-menu" />
             </Button>
-            <Dialog fullScreen open={dialogOpen} onClose={() => setDialogOpen(false)}>
-                <List>
+            <Dialog className="menu__dialog" fullScreen open={dialogOpen} onClose={() => setDialogOpen(false)}>
+            <AppBar sx={{ position: 'relative' }} >
+                <Toolbar>
+                    <Button variant="contained" color="primary" onClick={() => setDialogOpen(false)}> 
+                        <CloseIcon  role="close" />
+                    </Button>
+                </Toolbar>
+            </AppBar>
+                <List className="menu__items">
                     <ListItem autoFocus button >
                         <ListItemText primary="Mis Retos" />
                     </ListItem>                
